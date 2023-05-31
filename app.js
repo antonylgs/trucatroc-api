eec1f76c6de36bedd86ce997ef91e485e9a7f65b
antonylgs <100218623+antonylgs@users.noreply.github.com>
2023-05-31 12:54:02 +0200

    added CORS

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 require("./db");
 const apiRouter = require("./api");
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
